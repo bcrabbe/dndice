60c8bc53f15484705d12cd5cfe005e578d3990f2
bcrabbe <ben.crabbe.dev@gmail.com>
2020-11-16 22:05:06 +0000

    poll log

--- src/index.js
+++ src/index.js
@@ -7,40 +7,62 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 
 app.get('/', (req, res) => {
   res.send(`
-<div id="new">
-<script type="text/javascript">
-  const display = (result) => {
-    const tag = document.createElement("div");
-    const text = document.createTextNode(\`You rolled \$\{result\}\`);
-    tag.appendChild(text);
-    const element = document.getElementById("new");
-    element.appendChild(tag);
-  }
-  const roll = () => {
-    const request = new Request('/roll', {
-      method: 'POST',
-      body: JSON.stringify({ die: 'd20' }),
-      headers: new Headers({
-        'Content-Type': 'application/json'
-      })
+<script type="text/javascript" >
+const display = (log) => {
+  const textArea = document.getElementById("log")
+  textArea.textContent = log
+}
+
+const roll = () => {
+  const request = new Request('/roll', {
+    method: 'POST',
+    body: JSON.stringify({ die: 'd20' }),
+    headers: new Headers({
+      'Content-Type': 'application/json'
     })
+  })
 
   fetch(request)
     .then(response => {
       if (response.status === 200) {
-        return response.json();
+        return response.json()
       } else {
-        throw new Error('Something went wrong on api server!');
+        throw new Error('Something went wrong on api server!')
       }
     })
-    .then(({ result, log }) => {
-      display(result, log)
+    .then(({ log }) => {
+      display(log)
     }).catch(error => {
-      console.log(error);
+      console.log(error)
     })
 }
+
+const poll = () => {
+  const request = new Request('/poll', {
+    method: 'GET',
+  })
+
+  fetch(request)
+    .then(response => {
+      if (response.status === 200) {
+        return response.json()
+      } else {
+        throw new Error('Something went wrong on api server!')
+      }
+    })
+    .then(({ log }) => {
+      display(log)
+    }).catch(error => {
+      console.log(error)
+    })
+}
+
+poll()
+setInterval(poll, 1000)
 </script>
-<input type="button" value="Click me" onClick='roll()' id='roller'>
+<div>
+   <input type="button" value="Click me" onClick='roll()' id='roller'>
+   <div id="log" />
 </div>`)
 })
 
@@ -51,6 +73,10 @@ app.post('/roll', (req, res) => {
   const rolled = roll(die)
   rollLog.push(rolled)
   res.send({result: rolled, log: rollLog})
+})
+
+app.get('/poll', (req, res) => {
+  res.send({log: rollLog})
 })
 
 const roll = (n) => {
