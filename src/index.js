732a66bdc0eb210b379d14baf286264bd36f5ac8
Elliot Hogg <elliothogg@live.com>
2020-11-17 13:08:33 +0000

    added d8,d6 dice and clear log button

--- src/index.js
+++ src/index.js
@@ -20,11 +20,11 @@ const display = (log) => {
 
 const getName = () => document.getElementById("name").value
 
-const roll = () => {
+const roll = (n) => {
   const name = getName()
   const request = new Request('/roll', {
     method: 'POST',
-    body: JSON.stringify({ die: 'd20', name }),
+    body: JSON.stringify({ die: n, name }),
     headers: new Headers({
       'Content-Type': 'application/json'
     })
@@ -65,22 +65,47 @@ const poll = () => {
     })
 }
 
+const clearLog = () => {
+  const request = new Request('/clearLog', {
+    method: 'POST',
+  })
+
+  fetch(request)
+    .then(response => {
+      if (response.status === 200) {
+        return response.json()
+      } else {
+        throw new Error('Something went wrong on api server!')
+      }
+    })
+    .then(({ log }) => {
+      display(log)
+    }).catch(error => {
+      console.log(error)
+    })
+}
+
 poll()
 setInterval(poll, 1000)
 
 </script>
+<div>  <label for="name">Enter your name:</label </div>
 <div>
-   <input id="name" type=text placeholder="enter your name.."/>
-   <input type="button" value="Click me" onClick='roll()' id='roller' />
+   <input id="name" type=text />
+   <input type="button" value="Roll D20" onClick='roll("d20")' id='roller' />
+   <input type="button" value="Roll D8" onClick='roll("d8")' id='roller' />
+   <input type="button" value="Roll D6" onClick='roll("d6")' id='roller' />
+   <input type="button" value="Clear Log" onClick='clearLog();poll()' id='roller' />
    <div id="log" />
 </div>`)
 })
 
-const rollLog = []
+var rollLog = []
 
 app.post('/roll', (req, res) => {
   const { body: { die, name } } = req
   const rolled = roll(die)
+  const time = getTime()
   rollLog.push({ name, rolled, die })
   res.send({result: rolled, log: rollLog})
 })
@@ -89,11 +114,25 @@ app.get('/poll', (req, res) => {
   res.send({log: rollLog})
 })
 
+app.post('/clearLog', (req, res) => {
+  req = clearLog()
+})
+
 const roll = (n) => {
   [rolls, outOf] = n.split('d')
   console.log(rolls)
   return Math.floor(Math.random() * Math.floor(parseInt(outOf))) + 1
 }
+
+const clearLog = () => {
+  rollLog = [];
+}
+
+const getTime = () => {
+  const dateNow = new Date()
+  return dateNow.getHours() + ":" + dateNow.getMinutes()
+}
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
